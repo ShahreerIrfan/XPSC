cfdf8cf2bbb23db5467f2b736f1cc03b179e4ee1
Your Name <mdshahreerirfan@gmail.com>
2023-10-19 23:41:35 +0600

    sub array

--- Live_class_01/sub_array_sum/sub_array_sum.cpp
+++ Live_class_01/sub_array_sum/sub_array_sum.cpp
@@ -3,13 +3,16 @@ using namespace std;
 int main(){
     int n; cin>>n;
     int arr[n];
+    int mx = INT_MIN;
     for(int i=0;i<n;i++)cin>>arr[i];
     int k;cin>>k;
     for(int i=0;i<=n-k;i++){
+        int s = 0;
         for(int j=i;j<i+k;j++){
-            cout<<arr[j]<<" ";
+            s+=arr[j];
         }
-        cout<<endl;
+        mx = max(mx,s);
     }
+    cout<<mx<<endl;
     
 }
